1be377eaebe704ab2d23aca2f333e13819bae196
jasonwang24 <j.wang2216@gmail.com>
2019-06-25 23:23:57 -0400

    built webscraping feature for TOR and GSW

--- webscrape.py
+++ webscrape.py
@@ -1,14 +1,24 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+import requests
+from lxml import html
+import csv
 import pandas as pd
 
-year="2019"
-team="TOR"
-url="https://stats.nba.com/teams/traditional/?sort=GP&dir=-1"
-html =urlopen(url)
-bsObj = BeautifulSoup(html,"lxml")
 
-new = bsObj.find_all("div",{"class":"nba-stat-table__overflow"})
+year='2019'
+teams=['TOR','GSW']
 
-print(new)
 
+
+
+
+
+for team in teams:
+    r = requests.get('https://www.basketball-reference.com/teams/' + team + '/' + year + '/gamelog/')
+    data = html.fromstring(r.text)
+    date = data.xpath("//tbody/tr/td[@data-stat='date_game']/a[@href]/text()")
+    teamPoints = data.xpath("//tbody/tr/td[@data-stat='pts']/text()")
+    oppPoints = data.xpath("//tbody/tr/td[@data-stat='opp_pts']/text()")
+    print(team)
+    print(date)
+    print(teamPoints)
+    print(oppPoints)
