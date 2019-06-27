13fea7e6b00a4d19aa905a987b38ff1058291625
jasonwang24 <j.wang2216@gmail.com>
2019-06-27 01:59:10 -0400

    store scraped values to csv

--- webscrape.py
+++ webscrape.py
@@ -2,23 +2,46 @@ import requests
 from lxml import html
 import csv
 import pandas as pd
+import csv
+
+year = '2019'
+team = 'TOR'
+
+csvFile = open('scrapedData.csv', 'w+', newline='')
+
+r = requests.get('https://www.basketball-reference.com/teams/' + team+ '/' + year + '/gamelog/')
+data = html.fromstring(r.text)
+
+length=len(data.xpath("//tbody/tr/td[@data-stat='date_game']/a[@href]/text()"))
+date = zip(data.xpath("//tbody/tr/td[@data-stat='date_game']/a[@href]/text()"))
+teamPoints = zip(data.xpath("//tbody/tr/td[@data-stat='pts']/text()"))
+oppPoints = zip(data.xpath("//tbody/tr/td[@data-stat='opp_pts']/text()"))
 
 
-year='2019'
-teams=['TOR','GSW']
+newDate=[]
+newTeamPts=[]
+newOppPts=[]
+currentTeam=[]
+
+rowLines=[]
+
+for i in date:
+    newDate.append(i)
+
+for i in teamPoints:
+    newTeamPts.append(i)
+
+for i in oppPoints:
+    newOppPts.append(i)
+
+try:
+    writer = csv.writer(csvFile,delimiter=',')
+    for i in range(length):
+        currentLine=newDate[i]+newTeamPts[i]+newOppPts[i]
+        rowLines.append(currentLine)
+
+    writer.writerows(rowLines)
 
 
-
-
-
-
-for team in teams:
-    r = requests.get('https://www.basketball-reference.com/teams/' + team + '/' + year + '/gamelog/')
-    data = html.fromstring(r.text)
-    date = data.xpath("//tbody/tr/td[@data-stat='date_game']/a[@href]/text()")
-    teamPoints = data.xpath("//tbody/tr/td[@data-stat='pts']/text()")
-    oppPoints = data.xpath("//tbody/tr/td[@data-stat='opp_pts']/text()")
-    print(team)
-    print(date)
-    print(teamPoints)
-    print(oppPoints)
+finally:
+    csvFile.close()
