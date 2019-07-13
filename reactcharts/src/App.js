2001638b5b1f502d7cd70bfcd0e6e9b73a56c1fd
jasonwang24 <j.wang2216@gmail.com>
2019-07-13 01:15:14 -0400

    made design for page header

--- reactcharts/src/App.js
+++ reactcharts/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import title from './title.svg';
+import raptors from './raptors.svg';
+import warriors from './warriors.svg';
 import './App.css';
 import Chart from './components/Chart';
 
 function App() {
   return (
     <div className="App">
+      <header className="Top">
+          <img src={title} />
+      </header>
       <header className="App-header">
+        <img src={raptors} className="App-raptors"/>
         <img src={logo} className="App-logo" alt="logo" />
-
+        <img src={warriors} className="App-warriors"/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
       <Chart />
